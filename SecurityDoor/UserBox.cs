34836d35bb10a927fad1c0bcb006c8f566a4ad39
Shamo Humbatli <51967485+shamo-humbatli@users.noreply.github.com>
2019-07-21 12:33:03 -0700

    Add files via upload

---
+++ SecurityDoor/UserBox.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityDoor
+{
+    public class UserBox
+    {
+        private String username;
+        private String password;
+
+        public UserBox()
+        {
+
+        }
+
+        public UserBox(String Username, String Password)
+        {
+            this.Username = Username;
+            this.Password = Password;
+        }
+
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+    }
+}
---
+++ SecurityDoor/UserCredentials.cs
@@ -0,0 +1,66 @@
+﻿using CredentialManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityDoor
+{
+    public class UserCredentials
+    {
+        private String targetName;
+
+        public string TargetName { get => targetName; set => targetName = value; }
+
+        public UserCredentials()
+        {
+            this.targetName = Assembly.GetEntryAssembly().GetName().Name;
+        }
+        
+        public bool SaveUserCredentials(String Username, String Password)
+        {
+            try
+            {
+                using (var cred = new Credential())
+                {
+                    cred.Username = Username;
+                    cred.Password = Password;
+                    cred.Target = this.targetName;
+                    cred.Type = CredentialType.Generic;
+                    cred.PersistanceType = PersistanceType.LocalComputer;
+                    cred.Save();
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public UserBox GetUserCredentials()
+        {
+            var cred = new Credential();
+            cred.Target = this.targetName;
+
+            if (!cred.Load())
+            {
+                return null;
+            }
+
+            return new UserBox(cred.Username, cred.Password);
+        }
+
+        public bool DeleteUserCredentials()
+        {
+            using (var cred = new Credential() { Target = targetName })
+            {
+                return cred.Delete();
+            }
+        }
+
+
+    }
+}
